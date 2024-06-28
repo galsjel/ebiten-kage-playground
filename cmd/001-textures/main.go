06202c3d36d3f6dc64047e9dd85cd5d503ff80d6
dane <dane@medieval.software>
2024-06-28 19:49:37 -0400

    fix(001): cleaned up code

--- cmd/001-textures/main.go
+++ cmd/001-textures/main.go
@@ -88,8 +88,8 @@ func main() {
 	}
 
 	game := &game{
-		image: ebiten.NewImageFromImage(image),
-		mesh:  mesh,
+		texture: ebiten.NewImageFromImage(image),
+		mesh:    mesh,
 		camera: camera{
 			yaw: math.Pi,
 			pos: vec3{0, 10, -10},
@@ -109,7 +109,7 @@ func main() {
 
 type game struct {
 	cycle     float32
-	image     *ebiten.Image
+	texture   *ebiten.Image
 	mesh      *mesh
 	frametime time.Duration
 	camera    camera
@@ -132,14 +132,39 @@ type camera struct {
 }
 
 type triangle struct {
-	v1, v2, v3 uint16
+	p1, p2, p3 uint16
 	t1, t2, t3 uint16
 }
 
 type mesh struct {
-	vertices  []vec3
 	triangles []triangle
+	points    []vec3
 	texcoords []vec2
+}
+
+type vertex struct {
+	position vec4
+	texcoord vec2
+}
+
+func interpolate_vec4(v1, v2, v3 vec4, f vec3) (result vec4) {
+	result = result.Add(v1.Mul(f.X()))
+	result = result.Add(v2.Mul(f.Y()))
+	result = result.Add(v3.Mul(f.Z()))
+	return
+}
+
+func interpolate_vec2(v1, v2, v3 vec2, f vec3) (result vec2) {
+	result = result.Add(v1.Mul(f.X()))
+	result = result.Add(v2.Mul(f.Y()))
+	result = result.Add(v3.Mul(f.Z()))
+	return
+}
+
+func interpolate_vertex(v1, v2, v3 vertex, f vec3) (result vertex) {
+	result.position = interpolate_vec4(v1.position, v2.position, v3.position, f)
+	result.texcoord = interpolate_vec2(v1.texcoord, v2.texcoord, v3.texcoord, f)
+	return
 }
 
 func load_obj(src []byte) (*mesh, error) {
@@ -163,7 +188,7 @@ func load_obj(src []byte) (*mesh, error) {
 			if _, err := fmt.Fscanf(reader, "%f %f %f", &x, &y, &z); err != nil {
 				return nil, fmt.Errorf("bad vertex: %w", err)
 			}
-			mesh.vertices = append(mesh.vertices, vec3{x, y, z})
+			mesh.points = append(mesh.points, vec3{x, y, z})
 		case "vt":
 			var s, t float
 			if _, err := fmt.Fscanf(reader, "%f %f", &s, &t); err != nil {
@@ -177,9 +202,9 @@ func load_obj(src []byte) (*mesh, error) {
 				return nil, fmt.Errorf("bad face: %w", err)
 			}
 			mesh.triangles = append(mesh.triangles, triangle{
-				v1: v1 - 1,
-				v2: v2 - 1,
-				v3: v3 - 1,
+				p1: v1 - 1,
+				p2: v2 - 1,
+				p3: v3 - 1,
 				t1: t1 - 1,
 				t2: t2 - 1,
 				t3: t3 - 1,
@@ -196,62 +221,6 @@ type viewport struct {
 	h   int
 	w_2 int
 	h_2 int
-}
-
-type context struct {
-	view_matrix mat4
-	proj_matrix mat4
-	near, far   float
-	viewport    viewport
-}
-
-func (c *context) set_viewport(x, y, w, h int) {
-	c.viewport.x = x
-	c.viewport.y = y
-	c.viewport.w = w
-	c.viewport.h = h
-	c.viewport.w_2 = w / 2
-	c.viewport.h_2 = h / 2
-}
-
-func (c *context) set_orthographic(left, right, bottom, top, near, far float32) {
-	c.proj_matrix = mgl32.Ortho(left, right, bottom, top, near, far)
-	c.near = near
-	c.far = far
-}
-
-func (c *context) set_perpsective(fov_y, aspect, near, far float) {
-	c.proj_matrix = mgl32.Perspective(fov_y, aspect, near, far)
-	c.near = near
-	c.far = far
-}
-
-func (c *context) look_at(eye, center, up vec3) {
-	c.view_matrix = mgl32.LookAtV(eye, center, up)
-}
-
-func (c *context) projection_view_matrix() mat4 {
-	return c.proj_matrix.Mul4(c.view_matrix)
-}
-
-func clip_out_of_bounds(a vec4) bool {
-	x, y, z, w := a.X(), a.Y(), a.Z(), a.W()
-	return x < -w || x > w || y < -w || y > w || z < -w || z > w
-}
-
-func (c *context) clip_to_ndc(src vec4) vec4 {
-	return src.Mul(1.0 / src.W())
-}
-
-func (c *context) ndc_to_screen(src vec4) vec4 {
-	w_2 := float(c.viewport.w_2)
-	h_2 := float(c.viewport.h_2)
-	return vec4{
-		w_2*src.X() + w_2,
-		h_2*src.Y() + h_2,
-		0.5*src.Z() + 0.5,
-		1,
-	}
 }
 
 func (self *game) Layout(outerWidth, outerHeight int) (int, int) {
@@ -382,157 +351,154 @@ func barycentric(p1, p2, p3, p vec3) vec3 {
 	return vec3{u, v, w}
 }
 
-func (self *game) Draw(screen *ebiten.Image) {
-	defer func(t time.Time) {
-		ft := time.Now().Sub(t)
-		if self.frametime == 0 {
-			self.frametime = ft
-		} else {
-			self.frametime += (ft - self.frametime) / 2
-		}
-	}(time.Now())
+type context struct {
+	view_matrix mat4
+	proj_matrix mat4
 
-	var ctx context
-	w := screen.Bounds().Dx()
-	h := screen.Bounds().Dy()
+	// viewport is used to convert normalized device coordinates to screen coordinates
+	viewport viewport
 
-	ctx.set_viewport(0, 0, w, h)
+	// statistics
+	drawn_triangles int
 
-	// If you use orthographic then the Z axis will invert for everything.
-	// https://www.songho.ca/opengl/gl_projectionmatrix.html#perspective
-	// ctx.set_orthographic(-eye_distance*game_aspect, eye_distance*game_aspect, eye_distance, -eye_distance, 0.1, 10)
+	// the following are not required to be stored here,
+	// they serve as buffers to reduce overall allocations.
 
-	ctx.set_perpsective(30, game_aspect, 0.1, 100)
+	clip_space_points []vec4
+	screen_triangles  []screen_triangle
+	vertices          []ebiten.Vertex
+	indices           []uint16
+}
 
-	// the camera view matrix is invalid until the user controls it
-	if self.camera.view_matrix.Det() == 0 {
-		ctx.look_at(
-			vec3{0, 10, -10},
-			vec3{0, 10, 10},
-			vec3{0, 1, 0},
-		)
-	} else {
-		ctx.view_matrix = self.camera.view_matrix
+type screen_triangle struct {
+	v1, v2, v3 vertex
+	distance   float
+}
+
+func (c *context) set_viewport(x, y, w, h int) {
+	c.viewport.x = x
+	c.viewport.y = y
+	c.viewport.w = w
+	c.viewport.h = h
+	c.viewport.w_2 = w / 2
+	c.viewport.h_2 = h / 2
+}
+
+func clip_out_of_bounds(a vec4) bool {
+	x, y, z, w := a.X(), a.Y(), a.Z(), a.W()
+	return x < -w || x > w || y < -w || y > w || z < -w || z > w
+}
+
+func (c *context) clip_to_ndc(src vec4) vec4 {
+	return src.Mul(1.0 / src.W())
+}
+
+func (c *context) ndc_to_screen(src vec4) vec4 {
+	w_2 := float(c.viewport.w_2)
+	h_2 := float(c.viewport.h_2)
+	z := 0.5*src.Z() + 0.5
+	return vec4{
+		w_2*src.X() + w_2,
+		h_2*src.Y() + h_2,
+		z,
+		1.0 / z,
+	}
+}
+
+func (ctx *context) push_mesh(mesh *mesh) {
+	// save us some calculations by doing this here instead of per point
+	projection_view_matrix := ctx.proj_matrix.Mul4(ctx.view_matrix)
+
+	// transform all the mesh points into clip space
+	for _, point := range mesh.points {
+		point := projection_view_matrix.Mul4x1(point.Vec4(1))
+		ctx.clip_space_points = append(ctx.clip_space_points, point)
 	}
 
-	projection_view_matrix := ctx.projection_view_matrix()
-
-	var vertices_clip_space []vec4
-	for _, vertex := range self.mesh.vertices {
-		vertex := projection_view_matrix.Mul4x1(vertex.Vec4(1))
-		vertices_clip_space = append(vertices_clip_space, vertex)
-	}
-
-	type vertex struct {
-		position vec4
-		texcoord vec2
-	}
-
-	interpolate_vec4 := func(v1, v2, v3 vec4, f vec3) (result vec4) {
-		result = result.Add(v1.Mul(f.X()))
-		result = result.Add(v2.Mul(f.Y()))
-		result = result.Add(v3.Mul(f.Z()))
-		return
-	}
-
-	interpolate_vec2 := func(v1, v2, v3 vec2, f vec3) (result vec2) {
-		result = result.Add(v1.Mul(f.X()))
-		result = result.Add(v2.Mul(f.Y()))
-		result = result.Add(v3.Mul(f.Z()))
-		return
-	}
-
-	interpolate_vertex := func(v1, v2, v3 vertex, f vec3) (result vertex) {
-		result.position = interpolate_vec4(v1.position, v2.position, v3.position, f)
-		result.texcoord = interpolate_vec2(v1.texcoord, v2.texcoord, v3.texcoord, f)
-		return
-	}
-
-	type screen_triangle struct {
-		v1, v2, v3 vertex
-		distance   float
-	}
-
-	var screen_triangles []screen_triangle
-
-	push_triangle := func(v1, v2, v3 vertex) {
-		ndc1 := ctx.clip_to_ndc(v1.position)
-		ndc2 := ctx.clip_to_ndc(v2.position)
-		ndc3 := ctx.clip_to_ndc(v3.position)
-
-		// back-face culling
-		if (ndc2.X()-ndc1.X())*(ndc3.Y()-ndc1.Y())-(ndc3.X()-ndc1.X())*(ndc2.Y()-ndc1.Y()) <= 0 {
-			return
-		}
-
-		v1.position = ctx.ndc_to_screen(ndc1)
-		v2.position = ctx.ndc_to_screen(ndc2)
-		v3.position = ctx.ndc_to_screen(ndc3)
-
-		screen_triangles = append(screen_triangles, screen_triangle{
-			v1:       v1,
-			v2:       v2,
-			v3:       v3,
-			distance: (v1.position.Z() + v2.position.Z() + v3.position.Z()) / 3,
-		})
-	}
-
-	for _, triangle := range self.mesh.triangles {
+	for _, triangle := range mesh.triangles {
 		v1 := vertex{
-			position: vertices_clip_space[triangle.v1],
-			texcoord: self.mesh.texcoords[triangle.t1],
+			position: ctx.clip_space_points[triangle.p1],
+			texcoord: mesh.texcoords[triangle.t1],
 		}
 		v2 := vertex{
-			position: vertices_clip_space[triangle.v2],
-			texcoord: self.mesh.texcoords[triangle.t2],
+			position: ctx.clip_space_points[triangle.p2],
+			texcoord: mesh.texcoords[triangle.t2],
 		}
 		v3 := vertex{
-			position: vertices_clip_space[triangle.v3],
-			texcoord: self.mesh.texcoords[triangle.t3],
+			position: ctx.clip_space_points[triangle.p3],
+			texcoord: mesh.texcoords[triangle.t3],
 		}
 
 		if clip_out_of_bounds(v1.position) || clip_out_of_bounds(v2.position) || clip_out_of_bounds(v3.position) {
-			points := sutherland_hodgman_3d(v1.position, v2.position, v3.position)
-
-			p1 := v1.position.Vec3()
-			p2 := v2.position.Vec3()
-			p3 := v3.position.Vec3()
-
-			for i := 2; i < len(points); i++ {
-				b1 := barycentric(p1, p2, p3, points[0].Vec3())
-				b2 := barycentric(p1, p2, p3, points[i-1].Vec3())
-				b3 := barycentric(p1, p2, p3, points[i].Vec3())
-
-				push_triangle(
-					interpolate_vertex(v1, v2, v3, b1),
-					interpolate_vertex(v1, v2, v3, b2),
-					interpolate_vertex(v1, v2, v3, b3),
-				)
-			}
+			ctx.clip_triangle_and_push(v1, v2, v3)
 		} else {
-			push_triangle(v1, v2, v3)
+			ctx.push_triangle(v1, v2, v3)
 		}
 	}
+}
 
-	var vertices []ebiten.Vertex
-	var indices []uint16
+func (c *context) clip_triangle_and_push(v1, v2, v3 vertex) {
+	points := sutherland_hodgman_3d(v1.position, v2.position, v3.position)
 
-	slices.SortFunc(screen_triangles, func(a, b screen_triangle) int {
+	p1 := v1.position.Vec3()
+	p2 := v2.position.Vec3()
+	p3 := v3.position.Vec3()
+
+	for i := 2; i < len(points); i++ {
+		b1 := barycentric(p1, p2, p3, points[0].Vec3())
+		b2 := barycentric(p1, p2, p3, points[i-1].Vec3())
+		b3 := barycentric(p1, p2, p3, points[i].Vec3())
+
+		c.push_triangle(
+			interpolate_vertex(v1, v2, v3, b1),
+			interpolate_vertex(v1, v2, v3, b2),
+			interpolate_vertex(v1, v2, v3, b3),
+		)
+	}
+}
+
+func (c *context) push_triangle(v1, v2, v3 vertex) {
+	ndc1 := c.clip_to_ndc(v1.position)
+	ndc2 := c.clip_to_ndc(v2.position)
+	ndc3 := c.clip_to_ndc(v3.position)
+
+	// back-face culling
+	if (ndc2.X()-ndc1.X())*(ndc3.Y()-ndc1.Y())-(ndc3.X()-ndc1.X())*(ndc2.Y()-ndc1.Y()) <= 0 {
+		return
+	}
+
+	v1.position = c.ndc_to_screen(ndc1)
+	v2.position = c.ndc_to_screen(ndc2)
+	v3.position = c.ndc_to_screen(ndc3)
+
+	c.screen_triangles = append(c.screen_triangles, screen_triangle{
+		v1:       v1,
+		v2:       v2,
+		v3:       v3,
+		distance: (v1.position.Z() + v2.position.Z() + v3.position.Z()) / 3,
+	})
+}
+
+func (ctx *context) sort_triangles() {
+	slices.SortFunc(ctx.screen_triangles, func(a, b screen_triangle) int {
 		if a.distance >= b.distance {
 			return -1
 		}
 		return 1
 	})
+}
 
-	tex_width := float(self.image.Bounds().Dx())
-	tex_height := float(self.image.Bounds().Dy())
+func (ctx *context) draw_triangles(texture, target *ebiten.Image) {
 
-	for _, triangle := range screen_triangles {
+	tex_width := float(texture.Bounds().Dx())
+	tex_height := float(texture.Bounds().Dy())
+
+	for _, triangle := range ctx.screen_triangles {
 		v1 := triangle.v1
 		v2 := triangle.v2
 		v3 := triangle.v3
 
-		vertices = append(vertices,
+		ctx.vertices = append(ctx.vertices,
 			ebiten.Vertex{
 				SrcX:   v1.texcoord.X() * tex_width,
 				SrcY:   v1.texcoord.Y() * tex_height,
@@ -565,15 +531,61 @@ func (self *game) Draw(screen *ebiten.Image) {
 			},
 		)
 
-		first_index := uint16(len(indices))
-		indices = append(indices, first_index, first_index+1, first_index+2)
+		first_index := uint16(len(ctx.indices))
+		ctx.indices = append(ctx.indices, first_index, first_index+1, first_index+2)
 	}
 
-	screen.DrawTriangles(vertices, indices, self.image, &ebiten.DrawTrianglesOptions{
+	target.DrawTriangles(ctx.vertices, ctx.indices, texture, &ebiten.DrawTrianglesOptions{
 		AntiAlias: true,
 	})
 
+	ctx.drawn_triangles = len(ctx.indices) / 3
+
+	// reset buffers
+	ctx.clip_space_points = ctx.clip_space_points[:0]
+	ctx.screen_triangles = ctx.screen_triangles[:0]
+	ctx.vertices = ctx.vertices[:0]
+	ctx.indices = ctx.indices[:0]
+}
+
+func (self *game) Draw(screen *ebiten.Image) {
+	defer func(t time.Time) {
+		ft := time.Now().Sub(t)
+		if self.frametime == 0 {
+			self.frametime = ft
+		} else {
+			self.frametime += (ft - self.frametime) / 2
+		}
+	}(time.Now())
+
+	var ctx context
+	w := screen.Bounds().Dx()
+	h := screen.Bounds().Dy()
+
+	ctx.set_viewport(0, 0, w, h)
+
+	// If you use orthographic then the Z axis will invert for everything.
+	// https://www.songho.ca/opengl/gl_projectionmatrix.html#perspective
+	// ctx.set_orthographic(-eye_distance*game_aspect, eye_distance*game_aspect, eye_distance, -eye_distance, 0.1, 10)
+
+	ctx.proj_matrix = mgl32.Perspective(30, game_aspect, 0.1, 100)
+
+	// the camera view matrix is invalid until the user controls it
+	if self.camera.view_matrix.Det() == 0 {
+		ctx.view_matrix = mgl32.LookAtV(
+			vec3{0, 10, -10},
+			vec3{0, 10, 10},
+			vec3{0, 1, 0},
+		)
+	} else {
+		ctx.view_matrix = self.camera.view_matrix
+	}
+
+	ctx.push_mesh(self.mesh)
+	ctx.sort_triangles()
+	ctx.draw_triangles(self.texture, screen)
+
 	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %.0f", ebiten.ActualTPS()), 0, 0)
 	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("FPS: %.0f (%v)", ebiten.ActualFPS(), self.frametime), 0, 14)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Triangles: %d", len(screen_triangles)), 0, 28)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Triangles: %d", ctx.drawn_triangles), 0, 28)
 }
